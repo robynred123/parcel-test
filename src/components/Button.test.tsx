2c8402cfcd114137ebbb562a62fd45c9228fa612
Robyn Pines <robyn.pines@asda.uk>
2022-05-12 16:55:13 +0100

    Improved unit test

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Button from './Button'
+import React from "react";
+import renderer from "react-test-renderer";
+import Button from "./Button";
 
-it('renders the button component successfully', () => {
-    const component = renderer.create(
-        <Button className='button' children='button' onClick={() => jest.fn()}/>
-    )
-    let testJson = component.toJSON();
-    expect(testJson).toMatchSnapshot()
-})
+const onClick = jest.fn();
+
+it("renders the button component successfully", () => {
+	const component = renderer.create(
+		<Button className="button" children="button" onClick={onClick} />
+	);
+	let testJson = component.toJSON();
+	renderer.act(() => testJson.props.onClick());
+	expect(testJson).toMatchSnapshot();
+	expect(onClick).toHaveBeenCalled();
+});
